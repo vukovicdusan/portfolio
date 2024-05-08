6ffa347f611606a2c86d183cd3f4e821a7e1d75e
dusan <dusan.v.vukovic@gmail.com>
2024-05-08 23:51:00 +0200

    Added nav to header

--- components/sections/Header.tsx
+++ components/sections/Header.tsx
@@ -2,7 +2,36 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-wrapperWidth p-wrapperPadding mx-auto">Header</header>
+    <header className="w-wrapperWidth p-wrapperPadding mx-auto">
+      <nav>
+        <ul className="flex gap-4 justify-center">
+          <li>
+            <a
+              className="text-quaternaryAccent hover:text-primaryAccent"
+              href="#websites"
+            >
+              Works
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-quaternaryAccent hover:text-primaryAccent"
+              href="#about"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              className="text-quaternaryAccent hover:text-primaryAccent"
+              href="#contact"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
